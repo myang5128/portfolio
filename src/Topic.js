7a46bbb906b1c3ea8a3c96fcdcb79a694f88cb6d
myang5128 <michaelsteves2001@gmail.com>
2023-01-07 18:45:16 -0500

    test

--- src/Topic.js
+++ src/Topic.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Topic.css";
 
 function Topic({ title, relink }) {
-    if (title == "Github" || "Instagram" || "LinkedIn" || "Handshake") {
+    if (title === "Github" || "Instagram" || "LinkedIn" || "Handshake") {
         return (
             <a href={relink} target="_blank" rel="noopener noreferrer">
                 <h4>{title}</h4>
